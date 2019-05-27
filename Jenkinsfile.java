6756af634bf40c4e02931a68e7bae18aa5a26a23
Luo Lanyingzhu (IFAG ATV MC D PTE EFE PA) <Lanyingzhu.Luo@infineon.com>
2019-05-27 11:35:50 +0200

    change step tp bat for execution

--- Jenkinsfile.java
+++ Jenkinsfile.java
@@ -1,10 +1,10 @@
-pipline {
-	agent { docker { image 'maven:3.3.3' } }
-	stages {
-	stage('build') {
-		steps {
-			sh 'mvn --version'
-			}
-		}
-	}
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                bat 'set'
+            }
+        }
+    }
 }
